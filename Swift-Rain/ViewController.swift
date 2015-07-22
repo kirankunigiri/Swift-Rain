02c368f9d09b5c29daf64fb0ae1b14383ed48803
Kiran <kunigirik@yahoo.com>
2015-07-22 13:37:58 -0700

    Initial Setup
    Created a blank Xcode project.

---
+++ Swift-Rain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Rain
+//
+//  Created by Kiran Kunigiri on 7/22/15.
+//  Copyright (c) 2015 Kiran Kunigiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
